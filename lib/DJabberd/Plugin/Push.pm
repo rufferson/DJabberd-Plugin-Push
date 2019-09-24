b21348d5bb3c611f800dd93c4b9ac4466758d7dd
Ruslan N. Marchenko <me@ruff.mobi>
2019-09-24 13:11:57 +0200

    Add response handlers, fix namespace propagation, retab

--- lib/DJabberd/Plugin/Push.pm
+++ lib/DJabberd/Plugin/Push.pm
@@ -5,7 +5,8 @@ use strict;
 use base 'DJabberd::Plugin';
 
 use constant {
-	PSHNv0 => "urn:xmpp:push:0",
+        PSHNv0 => "urn:xmpp:push:0",
+        PUBSUB => 'http://jabber.org/protocol/pubsub'
 };
 
 our $logger = DJabberd::Log->get_logger();
@@ -27,7 +28,7 @@ our $VERSION = '0.01';
 Implements XEP-0357 Push Notifications server part - enable/disable and publish.
 
     <VHost mydomain.com>
-	<Plugin DJabberd::Plugin::Push />
+        <Plugin DJabberd::Plugin::Push />
     </VHost>
 
 =head1 METHODS
@@ -44,52 +45,65 @@ sub run_before {
 
 my %callmap = (
     'set-{'.PSHNv0.'}enable' => \&enable,
-    'set-{'.PSHNv0.'}disable' => \&disable,
-    'error-{'.PSHNv0.'}pubsub' => \&error
+    'set-{'.PSHNv0.'}disable' => \&disable
+);
+my %respmap = (
+    'error-{jabber:server}error' => \&error,
+    'result-(BOGUS)' => \&result
 );
 sub register {
     my ($self,$vhost) = @_;
     my $manage_cb = sub {
-	my ($vh, $cb, $iq) = @_;
-	if(exists $callmap{$iq->signature} && (!$iq->to || $vh->uses_jid($iq->to))) {
-	    $callmap{$iq->signature}->($self,$iq);
-	    my $xml = "<iq type='result' id='".$iq->id."' to='".$iq->from."'/>";
-	    $iq->connection->log_outgoing_data($xml);
-	    $iq->connection->write(\$xml);
-	    return $cb->stop_chain;
-	}
-	$cb->decline;
+        my ($vh, $cb, $iq) = @_;
+        if(exists $callmap{$iq->signature} && (!$iq->to || $vh->uses_jid($iq->to))) {
+            $callmap{$iq->signature}->($self,$iq);
+            my $xml = "<iq type='result' id='".$iq->id."' to='".$iq->from."'/>";
+            $iq->connection->log_outgoing_data($xml);
+            $iq->connection->write(\$xml);
+            return $cb->stop_chain;
+        }
+        $cb->decline;
     };
     my $handle_cb = sub {
-	my ($vh,$cb,$sz) = @_;
-	if($sz->isa('DJabberd::Message') && $sz->from && $sz->to) {
-	    my $type = $sz->attr('{}type');
-	    # Skip anything but chat, normal or error (well...)
-	    return $cb->decline
-		unless(!$type
-			or $type eq 'chat'
-			or $type eq 'normal'
-			or $type eq 'error');
-	    # Skip bodyless control messages
-	    return $cb->decline
-		unless(grep {
-			$_->element_name eq 'body' && $_->children
-		       } $sz->children_elements);
-	    $self->handle($sz);
-	}
-	$cb->decline;
+        my ($vh,$cb,$sz) = @_;
+        if($sz->isa('DJabberd::Message') && $sz->from && $sz->to) {
+            my $type = $sz->attr('{}type');
+            # Skip anything but chat, normal or error (well...)
+            return $cb->decline
+                unless(!$type
+                        or $type eq 'chat'
+                        or $type eq 'normal'
+                        or $type eq 'error');
+            # Skip bodyless control messages
+            return $cb->decline
+                unless(grep {
+                        $_->element_name eq 'body' && $_->children
+                       } $sz->children_elements);
+            $self->handle($sz);
+        }
+        $cb->decline;
+    };
+    my $reply_cb = sub {
+        my ($vh, $cb, $iq) = @_;
+        if(exists $respmap{$iq->signature}) {
+            return $cb->stop_chain
+                if($respmap{$iq->signature}->($self,$iq));
+        }
+        $cb->decline;
     };
     $self->{vhost} = $vhost;
     Scalar::Util::weaken($self->{vhost});
     # Inject management IQ handler
     $vhost->register_hook("c2s-iq",$manage_cb);
+    # Register S2S IQ Handler to catch responses
+    $vhost->register_hook("s2s-iq",$reply_cb);
     # Deliver hook will handle outgoing and incoming messages.
     $vhost->register_hook("deliver",$handle_cb);
     $vhost->register_hook("DiscoBare", sub {
-	my ($vh,$cb,$iq,$disco,$bare,$from,$ri) = @_;
-	if($disco eq 'info' && $bare->as_bare_string eq $from->as_bare_string) {
-	    $cb->addFeatures(PSHNv0);
-	}
+        my ($vh,$cb,$iq,$disco,$bare,$from,$ri) = @_;
+        if($disco eq 'info' && $bare->as_bare_string eq $from->as_bare_string) {
+            $cb->addFeatures(PSHNv0);
+        }
     });
     $self->{reg} = {};
 }
@@ -112,10 +126,12 @@ sub enable {
     my $node = $el->attr('{}node');
     my $form;
     if($el->first_element) {
-	$form = DJabberd::Form->new($el->first_element);
-	$form = undef if($form->form_type ne 'http://jabber.org/protocol/pubsub#publish-options');
+        $form = DJabberd::Form->new($el->first_element);
+        $logger->debug("Form element: ".$form->as_xml);
+        $logger->debug("Form type: ".$form->form_type);
+        $form = undef if($form->form_type ne 'http://jabber.org/protocol/pubsub#publish-options');
     }
-    $logger->debug("Enabling Push Notifications for ".$jid->as_string." to $srv as $node");
+    $logger->debug("Enabling Push Notifications for ".$jid->as_string." to $srv as $node with ".$form);
     $self->{reg}->{$jid->as_bare_string} = {} unless($self->{reg}->{$jid->as_bare_string});
     $self->{reg}->{$jid->as_bare_string}->{$jid->as_string} = [ $srv, $node, $form ];
 }
@@ -129,9 +145,9 @@ sub disable {
     my ($self,$iq) = @_;
     my $jid = $iq->connection->bound_jid;
     if($self->{reg}->{$jid->as_bare_string}) {
-	$logger->debug("Disabling Push Notifications for ".$jid->as_string." to ".$self->{reg}->{$jid->as_bare_string}->{$jid->as_string}->[0]);
-	delete $self->{reg}->{$jid->as_bare_string}->{$jid->as_string};
-	delete $self->{reg}->{$jid->as_bare_string} unless(keys(%{$self->{reg}->{$jid->as_bare_string}}));
+        $logger->debug("Disabling Push Notifications for ".$jid->as_string." to ".$self->{reg}->{$jid->as_bare_string}->{$jid->as_string}->[0]);
+        delete $self->{reg}->{$jid->as_bare_string}->{$jid->as_string};
+        delete $self->{reg}->{$jid->as_bare_string} unless(keys(%{$self->{reg}->{$jid->as_bare_string}}));
     }
 }
 
@@ -158,9 +174,9 @@ sub enabled {
     my ($self,$jid) = @_;
     return () unless(exists $self->{reg}->{$jid->as_bare_string} && ref($self->{reg}->{$jid->as_bare_string}));
     if(!$self->{strict} || $jid->is_bare) {
-	return values(%{$self->{reg}->{$jid->as_bare_string}});
+        return values(%{$self->{reg}->{$jid->as_bare_string}});
     } elsif($self->is_enabled($jid)) {
-	return ($self->{reg}->{$jid->as_bare_string}->{$jid->as_string});
+        return ($self->{reg}->{$jid->as_bare_string}->{$jid->as_string});
     }
     return ();
 }
@@ -174,17 +190,17 @@ bare and full jid of the user which enabled carbons.
 sub wrap {
     my ($self,$form,$from,$to,$node,@cust) = @_;
     my $ret = DJabberd::IQ->new('','iq',
-	{ '{}from' => $from, '{}to' => $to },
-	[ DJabberd::XMLElement->new('http://jabber.org/protocol/pubsub','pubsub',{},[
-	    DJabberd::XMLElement->new(undef,'publish',{node=>$node},[
-		DJabberd::XMLElement->new(undef,'item',{},[
-		    DJabberd::XMLElement->new(PSHNv0,'notification',{},[ $form ])
-		])
-	    ])
-	  ])
-	]
+        { '{}from' => $from, '{}to' => $to, '{}type' => 'set'},
+        [ DJabberd::XMLElement->new(PUBSUB,'pubsub',{xmlns=>PUBSUB}, [
+            DJabberd::XMLElement->new(PUBSUB,'publish',{node=>$node},[
+                DJabberd::XMLElement->new(PUBSUB,'item',{},[
+                    DJabberd::XMLElement->new(PSHNv0,'notification',{xmlns=>PSHNv0},[],$form->as_xml())
+                ])
+            ])
+          ])
+        ]
     );
-    $ret->set_attr('{}id', "pshn-iq-".(10*$self->{idseq}++)+int(rand(10)));
+    $ret->set_attr('{}id', "pshn-iq-".((10*$self->{idseq}++)+int(rand(10))));
     return $ret;
 }
 
@@ -206,18 +222,52 @@ sub handle {
     return unless(@to);
     $logger->debug("Pushing to ".join(', ',@to));
     foreach my$push(@to) {
-	my $form = DJabberd::Form->new('submit',[
-	    {var=>'FORM_TYPE', value=>['urn:xmpp:push:summary'], type=>'hidden'},
-	    {var=>'message-count',value=>[1]},
-	    {var=>'last-message-sender',value=>[$from->as_bare_string]},
-	    {var=>'last-message-body',value=>['yes']}
-	]);
-	my $pub = $self->wrap($form,$to->as_bare_string,$push->[0],$push->[1]);
-	if($push->[2] && ref($push->[2])) {
-	    $pub->first_element->push_child(DJabberd::XMLElement->new(undef,'publish-options',{},[$push->[2]]));
-	}
-	$pub->deliver($self->vh);
+        my $form = DJabberd::Form->new('submit',[
+            {var=>'FORM_TYPE', value=>['urn:xmpp:push:summary'], type=>'hidden'},
+            {var=>'message-count',value=>[1]},
+            {var=>'last-message-sender',value=>[$from->as_bare_string]},
+            {var=>'last-message-body',value=>['yes']}
+        ]);
+        my $pub = $self->wrap($form,$self->vh->server_name,$push->[0],$push->[1]);
+        if($push->[2] && ref($push->[2])) {
+            $pub->first_element->push_child(DJabberd::XMLElement->new(undef,'publish-options',{},[],$push->[2]->as_xml));
+        } else {
+            $logger->debug("No push options in ".$push->[2]." which is unusual");
+        }
+        # Add to pending response queue, should be cleaned by error/result response or timeout
+        $self->{idq}->{$pub->id} = [ $to->as_bare_string, $to->as_string, time, $push->[0] ];
+        $pub->deliver($self->vh);
+        # Schedule garbage collection in free slot
+        Danga::Socket->AddTimer(0,sub {
+            for my$id (keys(%{$self->{idq}})) {
+                if((time - $self->{idq}->{$id}->[2]) > 900) {
+                    # No ack/nack in 15min? Garbage
+                    delete $self->{idq}->{$id};
+                }
+            }
+        });
     }
+}
+
+sub result {
+    my ($self, $iq) = @_;
+    if($iq->to eq $self->vh->server_name && exists $self->{idq}->{$iq->{id}} && $self->{idq}->{$iq->{id}}->[3] eq $iq->from) {
+        $logger->debug("Got response: ".$iq->as_xml);
+        # Just discard pending reply
+        return delete $self->{idq}->{$iq->{id}};
+    }
+}
+
+sub error {
+    my ($self, $iq) = @_;
+    if(exists $self->{idq}->{$iq->id} && ref($self->{idq}->{$iq->id}) && ref($self->{reg}->{$self->{idq}->{$iq->id}->[0]})) {
+        $logger->debug("Got error: ".$iq->as_xml);
+        # Discard pending reply and push enablement
+        my $data = delete $self->{idq}->{$iq->id};
+        delete $self->{reg}->{$data->[0]}->{$data->[1]};
+        return 1;
+    }
+    return undef;
 }
 
 =head1 AUTHOR
